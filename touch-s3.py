52a890d46bbe0f766a01b07fef845d60322d4571
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-12-21 15:27:12 -0700

    i suck

--- touch-s3.py
+++ touch-s3.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import os
-
-f = fi
 import boto3
 
 target_bucket_name = "staging.vgtc.org"
+
 session = boto3.Session(aws_access_key_id=os.ENVIRON["aws_access_key_id"],
                         aws_secret_access_key=os.ENVIRON["aws_secret_access_key"])
 resource = session.resource('s3')
