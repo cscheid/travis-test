cc2ea12e5a1854c57561fa546f3ab3e0b30cf0cd
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-12-21 15:30:03 -0700

    .askjhd

--- touch-s3.py
+++ touch-s3.py
@@ -4,8 +4,8 @@ import boto3
 
 target_bucket_name = "staging.vgtc.org"
 
-session = boto3.Session(aws_access_key_id=os.ENVIRON["aws_access_key_id"],
-                        aws_secret_access_key=os.ENVIRON["aws_secret_access_key"])
+session = boto3.Session(aws_access_key_id=os.environ["aws_access_key_id"],
+                        aws_secret_access_key=os.environ["aws_secret_access_key"])
 resource = session.resource('s3')
 bucket = resource.Bucket(target_bucket_name)
 
