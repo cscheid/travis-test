8790d82873081db421042a6c70f828906eec0949
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-12-21 15:17:35 -0700

    from file, not string

--- touch-s3.py
+++ touch-s3.py
@@ -6,4 +6,5 @@ target_bucket_name = "staging.vgtc.org"
 session = boto3.Session(profile_name="default")
 resource = session.resource('s3')
 bucket = resource.Bucket(target_bucket_name)
-bucket.put_object(Key="very-sneakily-hidden-object", Body="yup, here.", ContentType="text/plain")
+
+bucket.put_object(Key="very-sneakily-hidden-object", Body=open("contents.txt"), ContentType="text/plain")
