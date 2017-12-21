b2ba63736de3507fe43bfae84a7ba7d21770c1f9
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-12-21 15:10:39 -0700

    right username now

--- touch-s3.py
+++ touch-s3.py
@@ -3,7 +3,7 @@ import boto3
 
 target_bucket_name = "staging.vgtc.org"
 
-session = boto3.Session(profile_name="cscheid_ieeevis")
+session = boto3.Session(profile_name="default")
 resource = session.resource('s3')
 bucket = resource.Bucket(target_bucket_name)
 bucket.put_object(Key="very-sneakily-hidden-object", Body="yup, here.", ContentType="text/plain")
